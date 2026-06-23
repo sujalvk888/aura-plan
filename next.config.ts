bf126ac01b91704a6c52f8c1f1bfec0631c174be
Sujal V Kanchan <sujal.vk888@gmail.com>
2026-06-23 10:06:25 +0530

    Configure next/image to allow Vercel Blob images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
